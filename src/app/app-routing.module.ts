74fd70a033f2cc43f18cb91a69cb8cf265633154
Rahul Pol <rahul.h.pol@gmail.com>
2020-03-18 17:59:58 +0530

    added login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
 import { EmployeeslistComponent } from "./components/employeeslist/employeeslist.component";
+import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "dashboard", pathMatch: "full" },
+  { path: "", redirectTo: "login", pathMatch: "full" },
+  { path: "login", component: LoginComponent },
   { path: "dashboard", component: DashboardComponent },
   { path: "employeeslist", component: EmployeeslistComponent }
 ];
@@ -13,4 +15,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { EmployeeslistComponent } from "./employeeslist/employeeslist.component";
+import { LoginComponent } from "./login/login.component";
 
 @NgModule({
-  declarations: [DashboardComponent, EmployeeslistComponent],
+  declarations: [DashboardComponent, EmployeeslistComponent, LoginComponent],
   imports: [CommonModule],
-  exports: [DashboardComponent, EmployeeslistComponent]
+  exports: [DashboardComponent, EmployeeslistComponent, LoginComponent]
 })
 export class ComponentsModule {}
