3fa0966484df8968dc7ee08e317ea2a0aaccae60
Rahul Pol <rahul.h.pol@gmail.com>
2020-03-19 11:15:49 +0530

    19-03-2020-11-15

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+
 import { DashboardComponent } from "./dashboard/dashboard.component";
 import { EmployeeslistComponent } from "./employeeslist/employeeslist.component";
 import { LoginComponent } from "./login/login.component";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
+
 import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { TextFieldModule } from "@angular/cdk/text-field";
 
 @NgModule({
   declarations: [DashboardComponent, EmployeeslistComponent, LoginComponent],
   imports: [
     CommonModule,
-    MatAutocompleteModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    TextFieldModule
   ],
   exports: [DashboardComponent, EmployeeslistComponent, LoginComponent]
 })
