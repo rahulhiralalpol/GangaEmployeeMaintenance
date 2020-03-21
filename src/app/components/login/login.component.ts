14686458df34c35ad9c1208d6536762029454d54
Rahul Pol <rahul.h.pol@gmail.com>
2020-03-21 17:15:57 +0530

    Edited Login Component for Registration

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -8,5 +8,30 @@ import { Component, OnInit } from "@angular/core";
 export class LoginComponent implements OnInit {
   constructor() {}
 
+  registermode = false;
+
   ngOnInit(): void {}
+
+  ChangeState() {
+    this.registermode = !this.registermode;
+    if (this.registermode) {
+      document.getElementById("LoginButton").innerText = "R E G I S T E R";
+      document.getElementById("StateButton").innerText =
+        "Already a User ? ..... Click here to Login.....";
+      document.getElementById("StateIcon").innerText = "edit";
+    } else {
+      document.getElementById("LoginButton").innerText = "L O G I N";
+      document.getElementById("StateButton").innerText =
+        "New User ? ..... Click here to Register.....";
+      document.getElementById("StateIcon").innerText = "vpn_key";
+    }
+  }
+
+  DoProcess() {
+    if (this.registermode) {
+      console.log("Do Registration");
+    } else {
+      console.log("Do Login");
+    }
+  }
 }
