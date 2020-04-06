28a8a172c11ef93abb49650c2595fa6428c96884
rahulhiralalpol <rahul.h.pol@gmail.com>
2020-04-06 20:18:55 +0530

    For Github Pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDUjFC1My4yNrsQayxlGzTdUEN8dHuta6M",
+    authDomain: "gangaemployeemaintenance.firebaseapp.com",
+    databaseURL: "https://gangaemployeemaintenance.firebaseio.com",
+    projectId: "gangaemployeemaintenance",
+    storageBucket: "gangaemployeemaintenance.appspot.com",
+    messagingSenderId: "349588553046",
+    appId: "1:349588553046:web:4f313bc56d6d2d098c2af7"
+  }
 };
